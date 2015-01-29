fc4951ca997d30ebafc455f2272464072aa1a321
A.J. Fraser <ajamiesonfraser@gmail.com>
2015-01-29 11:47:42 -0400

    built login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,7 +8,20 @@ angular.module('starter.controllers', [])
     Chats.remove(chat);
   }
 })
+    .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+      $scope.data = {};
 
+      $scope.login = function() {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+          $state.go('tab.dash');
+        }).error(function(data) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Login failed!',
+            template: 'Please check your credentials!'
+          });
+        });
+      }
+    })
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
